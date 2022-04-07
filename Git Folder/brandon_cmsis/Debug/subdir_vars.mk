b87642dbbf66b008f903f375890af86cdfd3f5cd
Brandon Martinez <86257728+Brandoni23@users.noreply.github.com>
2022-04-07 00:01:58 -0400

    Add files via upload

---
+++ Git Folder/brandon_cmsis/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LDS_SRCS += \
+../msp432p401r.lds 
+
+C_SRCS += \
+../cmsis.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./cmsis.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./cmsis.o \
+./system_msp432p401r.o 
+
+OBJS__QUOTED += \
+"cmsis.o" \
+"system_msp432p401r.o" 
+
+C_DEPS__QUOTED += \
+"cmsis.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../cmsis.c" \
+"../system_msp432p401r.c" 
+
+
