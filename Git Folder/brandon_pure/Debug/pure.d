b937708dabc33e445c89ec14bd448a2482bb7de9
Brandon Martinez <86257728+Brandoni23@users.noreply.github.com>
2022-04-07 14:46:23 -0400

    Add files via upload

---
+++ Git Folder/brandon_pure/Debug/pure.d
@@ -0,0 +1,1 @@
+pure.o: ../pure.c
---
+++ Git Folder/brandon_tidriverlib/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LDS_SRCS += \
+../msp432p401r.lds 
+
+C_SRCS += \
+../system_msp432p401r.c \
+../tidriverlib.c 
+
+C_DEPS += \
+./system_msp432p401r.d \
+./tidriverlib.d 
+
+OBJS += \
+./system_msp432p401r.o \
+./tidriverlib.o 
+
+OBJS__QUOTED += \
+"system_msp432p401r.o" \
+"tidriverlib.o" 
+
+C_DEPS__QUOTED += \
+"system_msp432p401r.d" \
+"tidriverlib.d" 
+
+C_SRCS__QUOTED += \
+"../system_msp432p401r.c" \
+"../tidriverlib.c" 
+
+
